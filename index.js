928365f2fbb564cbdd5eb51f1fb71a08efc9e0b6
Justin Searls <searls@gmail.com>
2018-01-11 15:44:22 -0500

    Add autoprefixer b/c I screwed up in a past life

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 require('babel-core/register')
 const fs = require('fs')
 const sass = require('node-sass')
+const postcss = require('postcss')
+const autoprefixer = require('autoprefixer')
 const reactHtmlEmail = require('react-html-email')
 const previewEmail = require('preview-email')
 const mailchimpify = require('mailchimpify')
 
 const newsletter = require('./newsletter')
 const assets = require('./assets')
-const css = sass.renderSync({
+const rawCss = sass.renderSync({
   data: fs.readFileSync('./newsletter.sass').toString(),
   indentedSyntax: true
 }).css.toString()
 
-const html = mailchimpify(reactHtmlEmail.renderEmail(newsletter({css, assets})))
-fs.writeFileSync('newsletter.html', html)
-previewEmail({html})
+postcss([autoprefixer]).process(rawCss, {from: undefined}).then(function (result) {
+  const css = result.toString()
+  const html = mailchimpify(reactHtmlEmail.renderEmail(newsletter({css, assets})))
+  fs.writeFileSync('newsletter.html', html)
+  previewEmail({html})
+})
+
