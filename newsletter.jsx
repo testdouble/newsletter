96d685ba0414c695e85bc0044a6ab978073a53af
Justin Searls <searls@gmail.com>
2018-01-11 10:18:05 -0500

    start styling a header

--- newsletter.jsx
+++ newsletter.jsx
@@ -1,23 +1,35 @@
 const React = require('react')
-const {Email, Item, Image, A, Row, Col} = require('react-html-email')
+const {Email, Box, Item, Image, A, Row, Col} = require('react-html-email')
 
 module.exports = ({css, assets}) =>
 
 <Email title='Test Double Intelligence Briefing' headCSS={css}>
   <Item className="header">
-    <A href='https://testdouble.com'>
-      <Image data-mc-edit='logo' alt='react'
-        src={assets.logo} width={104} height={92} />
-    </A>
+    <div className="strip"></div>
+    <div className="logo">
+      <A href='https://testdouble.com'>
+        <Image data-mc-edit='logo' alt='react'
+          src={assets.logo} width={52} height={46} />
+      </A>
+    </div>
+    <div className="title">
+      <h3 data-mc-edit='classification'>Unclassified//For Official Use Only</h3>
+      <h1 data-mc-edit='title'>Test Double Intelligence Briefing</h1>
+      <h2 data-mc-edit='subtitle'>2008//08//31</h2>
+    </div>
   </Item>
-  <Row className="content">
-    <Col>
-      Left!
-    </Col>
-    <Col>
-      Right!
-    </Col>
-  </Row>
+  <Item className="content">
+    <Box>
+      <Row>
+        <Col>
+          Left!
+        </Col>
+        <Col>
+          Right!
+        </Col>
+      </Row>
+    </Box>
+  </Item>
   <Item className="footer">
   </Item>
 </Email>
