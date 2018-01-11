7c1a01cb9107d1c118f96c974bb0682a24ccb5bf
Justin Searls <searls@gmail.com>
2018-01-11 09:21:27 -0500

    Start work on actually laying things out

--- newsletter.jsx
+++ newsletter.jsx
@@ -1,13 +1,24 @@
 const React = require('react')
-const {Email, Item, Image, A} = require('react-html-email')
+const {Email, Item, Image, A, Row, Col} = require('react-html-email')
 
 module.exports = ({css, assets}) =>
 
 <Email title='Test Double Intelligence Briefing' headCSS={css}>
-  <Item align="left">
+  <Item className="header">
     <A href='https://testdouble.com'>
-      <Image data-mc-edit='logo' alt='react' src={assets.logo} width={100} height={100} />
+      <Image data-mc-edit='logo' alt='react'
+        src={assets.logo} width={104} height={92} />
     </A>
+  </Item>
+  <Row className="content">
+    <Col>
+      Left!
+    </Col>
+    <Col>
+      Right!
+    </Col>
+  </Row>
+  <Item className="footer">
   </Item>
 </Email>
 
