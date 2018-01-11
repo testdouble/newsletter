52274bb18a4667317c2c8f17092b99e76008bf1a
Justin Searls <searls@gmail.com>
2018-01-11 16:11:38 -0500

    test out some styles, notes:
    * position: relative doesn't work at all on gmail, so find something else
    * image scaling isn't a thing, need to upload the exact pixel point image or it'll
    be massive in most clients

--- newsletter.jsx
+++ newsletter.jsx
@@ -1,5 +1,5 @@
 const React = require('react')
-const {Email, Box, Item, Image, A, Row, Col} = require('react-html-email')
+const {Email, Box, Item, Image, A, Span, Row, Col} = require('react-html-email')
 
 module.exports = ({css, assets}) =>
 
@@ -8,29 +8,76 @@ module.exports = ({css, assets}) =>
     <div className="strip"></div>
     <div className="logo">
       <A href='https://testdouble.com'>
-        <Image data-mc-edit='logo' alt='react'
-          src={assets.logo} width={52} height={46} />
+        <Image data-mc-edit='logo' src={assets.logo} width={52} height={46} />
       </A>
     </div>
     <div className="title">
       <h3 data-mc-edit='classification'>Unclassified//For Official Use Only</h3>
       <h1 data-mc-edit='title'>Test Double Intelligence Briefing</h1>
-      <h2 data-mc-edit='subtitle'>2008//08//31</h2>
+      <h2 data-mc-edit='subtitle'>2008.08.31</h2>
     </div>
   </Item>
-  <Item className="content">
+  <Item className='content'>
     <Box>
       <Row>
         <Col>
-          Left!
+          <div className='item story' data-mc-repeatable='left-col-story'>
+            <h3 data-mc-edit='left-col-story-title'>
+              I'm a fat small header
+            </h3>
+            <p data-mc-edit='left-col-story-blurb'>
+               I'm thin body text about this long to explain the thing and why
+               it's interesting and so forth
+            </p>
+            <span className='link' data-mc-edit='left-col-story-link'>
+              <A href='https://URL_GOES_HERE'>Read &#10513;</A>
+            </span>
+          </div>
         </Col>
         <Col>
-          Right!
+          <div className='item dossier'>
+            <div className='tab'>Agent Dossier</div>
+            <div className='agent'>
+              <Image data-mc-edit='dossier-pic' src='*|DOSSIER_PIC|*'
+                width={52} height={80} />
+              <div className='name'>
+                <h1 data-mc-edit='dossier-name'>Sterling Archer</h1>
+                <h2 data-mc-edit='dossier-00'>000</h2>
+              </div>
+            </div>
+            <p class='bio' data-mc-edit='dossier-bio'>
+              Sterling Malory Archer, known primarily as Archer, is the main
+protagonist of the eponymous show. He is considered to be the world's deadliest
+spy. His voice is provided by H. Jon Benjamin.
+            </p>
+            <span className='link' data-mc-edit='dossier-link'>
+              <A href='URL_GOES_HERE'>Continue &#10513;</A>
+            </span>
+          </div>
+
+          <div className='item story' data-mc-repeatable='right-col-story'>
+            <h3 data-mc-edit='right-col-story-title'>
+              I'm a fat small header
+            </h3>
+            <p data-mc-edit='right-col-story-blurb'>
+               I'm thin body text about this long to explain the thing and why
+               it's interesting and so forth
+            </p>
+            <span className='link' data-mc-edit='right-col-story-link'>
+              <A href='URL_GOES_HERE'>Read &#10513;</A>
+            </span>
+          </div>
         </Col>
       </Row>
     </Box>
   </Item>
-  <Item className="footer">
+  <Item>
+    <div className="footer">
+    <div className="fuse">&#10042;</div>
+    <div className="sign-off">
+      Copyright &copy; Test Double, LLC. All rights reserved.
+    </div>
+    </div>
   </Item>
 </Email>
 
