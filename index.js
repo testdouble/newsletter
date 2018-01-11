82122a52dba9a950655ce7bcc73124b560c70a4a
Justin Searls <searls@gmail.com>
2018-01-11 09:28:58 -0500

    Switch to sass. Life's too short

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('babel-core/register')
 const fs = require('fs')
+const sass = require('node-sass')
 const reactHtmlEmail = require('react-html-email')
 const previewEmail = require('preview-email')
 const mailchimpify = require('mailchimpify')
 
 const newsletter = require('./newsletter')
 const assets = require('./assets')
-const css = fs.readFileSync('./newsletter.css').toString()
+const css = sass.renderSync({
+  data: fs.readFileSync('./newsletter.sass').toString(),
+  indentedSyntax: true
+}).css.toString()
 
 const html = mailchimpify(reactHtmlEmail.renderEmail(newsletter({css, assets})))
 fs.writeFileSync('newsletter.html', html)
